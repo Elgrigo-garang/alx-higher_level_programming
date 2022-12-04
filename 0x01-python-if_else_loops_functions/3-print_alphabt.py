83a3cfc39954ec5dd8f2e467fb990612c76ab293
Elgrigo-garang <collinsmacket7@gmail.com>
2022-12-04 10:38:29 +0000

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!usr/bin/python3
-#author - elgrigo
-for latter in range(97, 123):
- if chr(letter) != 'q' and chr(letter) != 'e':
-  print('{}'.format(chr(letter)), end"")
+#!/usr/bin/python3
+for c in range(97, 123):
+    if chr(c) != 'q' and chr(c) != 'e':
+        print('{}'.format(chr(c)), end='')
