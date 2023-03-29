6c8745a853d5ec5abb471774f8a4574730892ca5
Elgrigo-garang <lubangacollinsm@gmail.com>
2023-03-29 03:27:05 -0700

    task2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define a aclass square."""
+
+class square:
+    """Represent square."""
+
+    def __int__(self, size):
+        """initialize a new square.
+        Args:
+        size (int): the size of a square.
+        """self.__size = size
