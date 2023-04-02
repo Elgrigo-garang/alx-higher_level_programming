62b47c32b065a9571193897e5cf090bdb0e18880
Elgrigo-garang <lubangacollinsm@gmail.com>
2023-04-02 14:38:59 -0700

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
@@ -12,4 +13,3 @@ class Square:
             size (int): The size of the new square.
         """
         self.__size = size
-
