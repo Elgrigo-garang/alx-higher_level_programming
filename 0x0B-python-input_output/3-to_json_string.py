8542a8d9cd3d3272955a82d4d4113d1ff92af4ea
Elgrigo-garang <collinsmackey7@gmail.com>
2023-04-26 03:07:50 -0700

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""defines a new string in json"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the json representation of a new string"""
+    return json.dumps(my-obj)
