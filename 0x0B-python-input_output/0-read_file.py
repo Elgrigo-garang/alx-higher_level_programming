72489a72f34bce03176de986d3885090978f7d87
elgrigo-garang <collinsmackey7@gmail.com>
2023-09-30 02:54:52 -0700

    add test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Define a text file-raeding function."""
+
 
 def read_file(filename=""):
-    """print the content of a UTF8 text file to stdout"""
-    with open(filename, encodings="utf-8") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
