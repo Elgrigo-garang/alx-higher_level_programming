3c1816e81aec8259c67d9c87dbeeb579e034a51e
elgrigo-garang <collinsmackey7@gmail.com>
2023-10-19 13:32:21 -0700

    01

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,20 +1,7 @@
--- Lists all genres of the database hbtn_0d_tvshows
--- not linked to the show Dexter.
--- Records are sorted by ascending genre name.
-SELECT DISTINCT `name`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
-
-       INNER JOIN `tv_shows` AS t
-       ON s.`show_id` = t.`id`
-       WHERE g.`name` NOT IN
-             (SELECT `name`
-                FROM `tv_genres` AS g
-	             INNER JOIN `tv_show_genres` AS s
-		     ON g.`id` = s.`genre_id`
-
-		     INNER JOIN `tv_shows` AS t
-		     ON s.`show_id` = t.`id`
-		     WHERE t.`title` = "Dexter")
- ORDER BY g.`name`;
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
